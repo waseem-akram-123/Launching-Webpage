7b33f29dde447c4fb35ff446647ff94a7751805f
WASEEM <waseemakram8660@gmail.com>
2024-10-17 20:36:52 +0530

    Add files via upload

---
+++ LAUNCHER/launcher.js
@@ -0,0 +1,17 @@
+// var countDownDate = new Date("oct 16, 2021 00;00;00").getTime();
+// var x = setInterval (function (){
+//     var now = new Date().getTime();
+//     var distance = countDownDate - now;
+
+//     var days = Math.floor (distance % (1000*60*60*24));
+//     var hours = Math.floor ((distance % (1000*60*60*24)) / (1000*60*60));
+//     var minutes = Math.floor ((distance % (1000*60*60)) / (1000*60));
+//     var seconds = Math.floor ((distance % (1000*60))/1000);
+
+//     document.querySelector ("#days").innerHTML = days;
+//     document.querySelector ("#hours").innerHTML = hours;
+//     document.querySelector ("#minutes").innerHTML = minutes;
+//     document.querySelector ("#seconds").innerHTML = seconds;
+
+// },1000);
+
